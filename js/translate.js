a4d6cc53bcd81ed0d998cf7385270b6d8bc1a9cf
Suresh_Gobi <sureshgobi34@gmail.com>
2025-03-07 11:22:15 +0530

    logo quiality change

--- js/translate.js
+++ js/translate.js
@@ -1,7 +1,7 @@
 function googleTranslateElementInit() {
   var googleTranslateElement = new google.translate.TranslateElement({
     pageLanguage: 'en',
-    includedLanguages: 'en,si,ta,fr,de,id,th,hi,ja',
+    includedLanguages: 'en,ar,ta,fr,de,id,th,hi,ja',
     layout: google.translate.TranslateElement.InlineLayout.SIMPLE
   }, 'google_translate_element');
 
